7daab4ea12b056181bad0269a3532b5fcc44a2de
DavidMer <dmerlinsky@stuy.edu>
2016-01-12 11:42:05 -0800

    Create amishArcade.java
    This where you pick your game.

---
+++ amishArcade.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class amishArcade{
+    public String s;
+    public String r;
+    public static void displayGames(){
+		String s= "Choose Your Game! clifford\n";
+		String r= "________________" +"\n" +"|               |"+"\n"+"| 1 MasterMind  |"+"\n"+"________________";
+		System.out.print(r);
+}
+	public static void main(String[] args){
+	    displayGames();
+}		
+}		
