6e7a36706d8e94309c284f7051c2f173d0f26c4c
Jackson Dey <Jackson@Jacksons-MacBook-Pro-2.local>
2016-01-25 00:45:51 -0500

    Done

--- Othello.java
+++ Othello.java
@@ -7,6 +7,9 @@ public class Othello extends Game{
     public final String[][] board = new String[8][8];
     int turnctr;
     boolean gameover;
+    int xctr;
+    int octr;
+    int nay;
     //READERS
     private InputStreamReader isr;
     private BufferedReader in;
@@ -21,7 +24,7 @@ public class Othello extends Game{
 	in = new BufferedReader( isr );
 	
 
-    }//end constructor
+    }//end constructor 
 
     public void fill(){//fill board
         for(int r = 0; r < 8; r ++){
@@ -29,6 +32,7 @@ public class Othello extends Game{
                 board[r][c] = "* ";
                 
             }
+            
         }
         //special cases for the board start
         board[3][3] = sides[0];
@@ -41,37 +45,41 @@ public class Othello extends Game{
         if( board[r][c] == sides[0] ) {
            board[r][c] = sides[1];
        }  
-        else{
+        else if(board[r][c] == sides[1]){
             board[r][c] = sides[0];
         }
     }
     public void move(int r, int c,int player){
         board[r][c]=sides[player];
     }
-    public boolean winCheck(){
+    public boolean wincheck(){
         int i=0;
         for(int c=0;c<8;c++){
             for(int r=0;r<8;r++){
-                if(board[c][r].equals("#") || board[c][r].equals("O")){
-                    return true;
+                if(board[c][r].equals("X") || board[c][r].equals("O")){
+                    i++;
                 }
             }
         }
+        if(i == 64){
+            return true;
+        }
         return false;
-    }
-    
+    }//all wrong
+    //================================FLIPS================================================================
     public void horflip(int r, int c, int player){
        int left = horCheckL(r, c, player);
        int right = horCheckR(r, c, player);
        if(!(left == -1)){
-       for(int le = 1; le < left; le++){
+       for(int le = 1; le <= left; le++){
            flip(r, c - le);
-       }
+            
+           }
        }
        if(!(right == -1)){
-       for(int re = 1; re < right; re++){
+       for(int re = 1; re <= right; re++){
            flip(r, c + re);
-       }
+          }
        }
     }//end hor
     
@@ -79,12 +87,12 @@ public class Othello extends Game{
         int up = vertCheckUp(r, c, player);
         int down = vertCheckDown(r, c, player);
         if(!(up == -1)){
-        for(int le = 1; le < up; le++){
+        for(int le = 1; le <= up; le++){
            flip(r - le, c);
         }
         }//end if
         if(!(down == -1)){
-       for(int re = 1; re < down; re++){
+       for(int re = 1; re <= down; re++){
            flip(r + re, c);
         }
         }//end if
@@ -97,36 +105,38 @@ public class Othello extends Game{
         int DL = diagCheckDL(r, c, player);
         int DR = diagCheckDR(r, c, player);
     if(!(UL == -1)){
-    for(int upl = 1; upl < UL; upl++){
+    for(int upl = 1; upl <= UL; upl++){
         flip(r - upl, c - upl);
     }//end for
     }//end if
     if(!(UR == -1)){
-    for(int upr = 1; upr < UL; upr++){
+    for(int upr = 1; upr <= UR; upr++){
         flip(r - upr, c + upr);
     }//end for
     }//end if
     
     if(!(DL == -1)){
-    for(int dol = 1; dol < UL; dol++){
+    for(int dol = 1; dol <= DL; dol++){
         flip(r + dol, c - dol);
     }//end for
     }//end if
     
-    if(!(DR ==-1)){
-    for(int dor = 1; dor < UL; dor++){
+    if(!(DR == -1)){
+    for(int dor = 1; dor <= DR; dor++){
         flip(r + dor, c + dor);
     }//end for
     }//end if
     
     }//end diag
-    
+    //=============================================FLIPCHECKERS====================================================
+    //====================== HOR CHECKS =====================================  
+
     public int horCheckL(int r, int c, int player){
         int val = 0;
         boolean check = false;
         for(int t = c - 1; t > 0; t--){
             if(board[r][t] == sides[player]){
-                val = t;
+                val = c - t - 1;//SWITCHED TO A -1 
                 check = true;
                 break;
                 
@@ -142,14 +152,14 @@ public class Othello extends Game{
     public int horCheckR(int r, int c, int player){
         int val = 0;
         boolean check = false;
-        for(int t = 0; t < 7 - r; t++){
+        for(int t = c + 1; t < 7; t++){
             if(board[r][t] == sides[player]){
-                val = t;
+                val = (t - c) - 1;
                 check = true;
                 break;
             }
         } 
-      if (check){
+        if (check){
             return val;
         }
         else{
@@ -157,12 +167,13 @@ public class Othello extends Game{
         }   
         
     }
+    //====================== VERT CHECKS =================================================  
     public int vertCheckDown(int r, int c, int player){
         int val = 0;
         boolean check = false;
-        for(int t = 0; t < 7 - c; t++){
-            if(board[r][t] == sides[player]){
-                val = t;
+        for(int t = r + 1; t < 7; t++){
+            if(board[t][c] == sides[player]){
+                val = t - r - 1;
                 check = true;
                 break;
             }
@@ -180,8 +191,8 @@ public class Othello extends Game{
         int val = 0;
         boolean check = false;
         for(int t = r - 1; t > 0; t--){
-            if(board[r][t] == sides[player]){
-                val = t;
+            if(board[t][c] == sides[player]){
+                val = r - t - 1;
                 check = true;
                 break;
                 
@@ -194,15 +205,15 @@ public class Othello extends Game{
             return -1;
         }
         
-        
+    //====================== DIAGONAL CHECKS (lml so many) =====================================    
     }
     public int diagCheckUR(int r, int c, int player){
-        int _r = r - 2;
-        int _c = c + 2;
+        int _r = r - 1;
+        int _c = c + 1;
         int val = 0;
         boolean check = false;
-        int loop = 1;
-        if(c > 5 || r < 2){
+        int loop = 0;
+        if(c > 5 || r < 2){//just wouldnt work -> makes sense
             val= -1;
         }//no matchee matchee out there
         
@@ -221,17 +232,17 @@ public class Othello extends Game{
         }
         else{
             return -1;
-        }    
+        }    //checked!
         
         
         
     }
     public int diagCheckUL(int r, int c, int player){
-        int _r = r - 2;
-        int _c = c - 2;
+        int _r = r - 1;
+        int _c = c - 1;
         int val = 0;
         boolean check = false;
-        int loop = 1;
+        int loop = 0;
         if(c < 2 || r < 2){
             val= -1;
         }
@@ -255,15 +266,15 @@ public class Othello extends Game{
     }
     
     public int diagCheckDR(int r, int c, int player){
-        int _r = r + 2;
-        int _c = c + 2;
+        int _r = r + 1;
+        int _c = c + 1;
         int val = 0;
         boolean check = false;
-        int loop = 1;
+        int loop = 0;
         if(c > 5 || r > 5){
             val = -1;
         }//no matches over there
-        while( _r < 8 && _c <8 && check == false) {
+        while( _r < 8 && _c < 8 && check == false) {
             if(board[_r][_c] == sides[player]){
                 val = loop;
                 check = true;
@@ -282,11 +293,11 @@ public class Othello extends Game{
         
     }
     public int diagCheckDL(int r, int c, int player){
-        int _r = r + 2;
-        int _c = c - 2;
+        int _r = r + 1;
+        int _c = c - 1;
         int val = 0;
         boolean check = false;
-        int loop = 1;
+        int loop = 0;
          if(c < 2 || r > 5){
             return -1;
         }//sike no matches
@@ -312,8 +323,14 @@ public class Othello extends Game{
     
     public String toString(){
         
-	String rowerino="";
+	String rowerino="  ";
 	for(int i=0;i<8;i++){
+	    rowerino+=i+" ";
+	}
+	rowerino+="\n";
+    
+	for(int i=0;i<8;i++){
+	    rowerino+=i+" ";
 	    for(int c=0; c<8; c++){
 		rowerino += "" + board[i][c];
 	    }
@@ -330,25 +347,37 @@ public class Othello extends Game{
 	    vertflip(r, c, player);
 	    diagflip(r, c, player);
 	}
-	
+	public void counts(){
+	    for(int r = 0; r < 8; r++){
+	        for(int c = 0; c < 8; c++){
+	            if(board[r][c] == sides[0]){
+	                //if its an X
+	                xctr++;
+	            }
+	            if(board[r][c] == sides[1]){
+	                octr++;
+	            }  
+	                
+	        }//end 2nd for
+	    }//end first for
+	}//end counts
+
+	/*====================LETS PLAY=========================*/
     public void play(){
         String choice = "";
         String one = "1";
         String two = "2";
         int c = 0;
         int r = 0;
+        int c2 = 0; 
+        int r2 = 0;
         System.out.println("Lets get to flipping around!");
         System.out.println( this );
-        System.out.println( "One player(1) or two?(2)");
-        try{
-            choice = in.readLine();
-            }
-            catch( IOException e ) { }
-        if(choice.equals(one)){//one player
-        //oyve
-        }//end one player
-        if(choice.equals(two)){
+        System.out.println( "Make sure you have a friend cuz its two player showdown!");
+        
+        
             System.out.println("Two it is then!");
+            while(turnctr < 61){
             System.out.println("Ok player one, you're X");
             System.out.println("Pick your column!");
             try{
@@ -365,23 +394,72 @@ public class Othello extends Game{
             System.out.println(this);
             System.out.println("Lets see what flips!");
             try{
-                Thread.sleep(1000);//pauses for a second
+                Thread.sleep(800);//pauses for a bit
             }
             catch(InterruptedException e) { }
             masterFlip(r, c, 0);
             System.out.print(this);
+            //=============PLAYER TWO'S TURN===============================
+            System.out.println("Ok player twooo!  Remember, you're O");
+            System.out.println("Pick a column kido");
+            try{
+             c2 = Integer.parseInt(in.readLine());
+            }
+            catch( IOException e ) { }
+            System.out.println("And now a row laddy");
+            try{
+             r2 = Integer.parseInt(in.readLine());
+            }
+            catch( IOException e ) { }
+            board[r2][c2] = sides[1];
+            System.out.println(this);
+            System.out.println("What are we gonna do now?  We gonna flip!");
+            try{
+                Thread.sleep(800);//pauses for a bit
+            }
+            catch(InterruptedException e) { }
+            masterFlip(r2, c2, 1);
+            System.out.print(this);
             
-            
-            }//end two player    
-        
+            turnctr += 2;//cuz it was two turns
+                }//end while
+                System.out.println("The results are in:");
+                try{
+                Thread.sleep(1000);//pauses for a second
+                }
+                catch(InterruptedException e) { }//dramatic effect
+                System.out.println("Tallying.....");
+                try{
+                Thread.sleep(1000);//pauses for a second
+                }
+                catch(InterruptedException e) { }
+                System.out.println("Annnd.....");
+                try{
+                Thread.sleep(1000);//pauses for a second
+                }
+                catch(InterruptedException e) { }
+                
+                if(xctr > octr){
+                    System.out.println("Player 1 demolished player 2!");
+                    
+                    
+                }
+                else{
+                    System.out.println("Player 2 triumphs over player 1");
+                }
+                System.out.println("The score was " + xctr + " to " + octr + ".  Great game!");
+                System.out.println("Wanna play some more games? Yee(1) or naw(2)");
+                try{
+                nay = Integer.parseInt(in.readLine());
+                }
+                catch( IOException e ) { }
+                if(nay == 1){
+                amishArcade.displayGames();
+
+            }
+                
+    
     }//end play
-    
-    
-    
-    public boolean wincheck(){
-        return gameover;
-    }
-    
     
     public static void main(String[] args){
         Othello game = new Othello();
